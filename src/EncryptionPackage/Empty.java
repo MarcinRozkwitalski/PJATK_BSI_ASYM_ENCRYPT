26014bf1b3ade5d2201afa64973d8116a22f1845
TrojanFi <filip23t@gmail.com>
2020-12-10 17:44:56 +0100

    Des encryption 90% Done

---
+++ src/EncryptionPackage/Empty.java
@@ -0,0 +1,4 @@
+package EncryptionPackage;
+
+public class Empty {
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,27 @@
 package Main;
 
+
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import static EncryptionPackage.Des.Des.encryption;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Zaczynamy !!");
+    public static void main(String[] args) throws InvalidKeySpecException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+        File plaintext = new File("C:\\Moje pierdoły\\Materiały_PJWSTK_semestr_5\\BSI\\PJATK_BSI_ENCRYPTION\\src\\Files\\text.txt");
+        File encrypted = new File("C:\\Moje pierdoły\\Materiały_PJWSTK_semestr_5\\BSI\\PJATK_BSI_ENCRYPTION\\src\\Files\\NewText.txt");
+
+        try {
+            encryption("12345678", Cipher.ENCRYPT_MODE, plaintext, encrypted);
+            System.out.println("Encryption completed");
+        }catch (NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | IOException exception){
+            exception.printStackTrace();
+        }
     }
 }
