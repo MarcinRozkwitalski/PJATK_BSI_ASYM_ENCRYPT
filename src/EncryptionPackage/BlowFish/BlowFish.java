3bd1cfadeff67f24d99eea916890e85657e4fd63
MarcinRozkwitalski <marcinrozkwitalski98@gmail.com>
2020-12-15 12:40:41 +0100

    Added AES algorithm and switch cases
    Added AES algorithm and switch cases

--- src/EncryptionPackage/BlowFish/BlowFish.java
+++ src/EncryptionPackage/BlowFish/BlowFish.java
@@ -1,10 +1,14 @@
 package EncryptionPackage.BlowFish;
 
+import Main.Main;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.util.Scanner;
 
 public class BlowFish {
+
     public static void BlowFishTest(String input){
         try{
             KeyGenerator keyGenerator = KeyGenerator.getInstance("BlowFish");
@@ -21,6 +25,39 @@ public class BlowFish {
 
         }catch (Exception exception){
 
+        }
+    }
+
+    private static void DoDecrypt() {
+        //kod filipa
+    }
+
+    private static void DoEncrypt() {
+        //kod filipa
+    }
+
+    public static void main() {
+        System.out.println("1. Encrypt\n2. Decrypt\n3. Exit algorithm");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+
+        switch (option){
+            case 1:
+                System.out.println("You chose encryption.");
+                DoEncrypt();
+                break;
+            case 2:
+                System.out.println("You chose decryption.");
+                DoDecrypt();
+                break;
+            case 3:
+                System.out.println("You are leaving BlowFish algorithm.");
+                Main.TaskOptions();
+                break;
+            default:
+                System.out.println("This option doesn't exist.");
+                System.out.println("Please choose new option.");
+                main();
         }
     }
 }
